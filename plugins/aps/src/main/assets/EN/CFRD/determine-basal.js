2ff1ab7c8e3b8019e4709d40281be6f011e48b1b
DICKO <me@myplace.com>
2024-07-24 14:50:00 +1000

    SAFETY: Calculate rate restricting to remaining maxBolus or remaining insulinReq differential

--- plugins/aps/src/main/assets/EN/CFRD/determine-basal.js
+++ plugins/aps/src/main/assets/EN/CFRD/determine-basal.js
@@ -1938,7 +1938,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
                 // SAFETY: Calculate rate restricting to remaining maxBolus or remaining insulinReq differential
                 //rate = Math.min(maxBolus - microBolus, insulinReq * (insulinReqPct_orig-insulinReqPct));
-                rate = insulinReq * insulinReqPct_orig - microBolus;
+                rate = Math.min(maxBolus - microBolus, (insulinReq * insulinReqPct_orig) - microBolus);
                 // if AAPS original insulinReq is higher allow allow remaining insulinReqPct as TBR
                 //if (insulinReqOrig >= ENMaxSMB && ENactive) {
                 if (insulinReqOrig > 0 && ENactive) {
